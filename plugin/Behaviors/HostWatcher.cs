337f1f374f5af1d54af50a3bc1c39ef282380f17
PintTheDragon <PintTheDragon@hotmail.com>
2026-06-28 14:48:53 -0700

    Fix building and item sync

--- plugin/Behaviors/HostWatcher.cs
+++ plugin/Behaviors/HostWatcher.cs
@@ -40,55 +40,99 @@ internal class HostWatcher : MonoBehaviour
         if (curID == CSteamID.Nil.m_SteamID || oldID == CSteamID.Nil.m_SteamID) return;
 
         if (curID == KSteam.GetLocalUserSteamID().m_SteamID)
-        {
-            // We need to take over as the host.
-
-            ServerMain._RegisterServerReceivers();
-
-            Net.type = Net.NetType.Host;
-
-            var ply = transportSteamworks.SteamIDToNetPlayerDict[KSteam.GetLocalUserSteamID().m_SteamID];
-            ply.is_host = true;
-            ply.is_local = true;
-            ply.server_plrstate = new Server_PlayerState(ply);
-
-            NetPlayer.LOCAL_PLAYER = ply;
-
-            // TODO: Figure out a better place to spawn players.
-            Body_PlaceBody_MultiplayerPatch.has_spawn_location = true;
-            Body_PlaceBody_MultiplayerPatch.spawnlocation = ply.body.transform.position;
-
-            HardcodedServer.CurHostID = ply.clientId;
-
-            if (transportSteamworks.listenSocket != HSteamListenSocket.Invalid)
-            {
-                if (transportSteamworks.is_steamserver)
-                    SteamGameServerNetworkingSockets.CloseListenSocket(transportSteamworks.listenSocket);
-                else
-                    SteamNetworkingSockets.CloseListenSocket(transportSteamworks.listenSocket);
-            }
-
-            transportSteamworks.CloseP2PSessions();
-
-            transportSteamworks.CreateServerSocket();
-        }
+            SwitchToHost(transportSteamworks);
         else
-        {
-            // There's a new host we need to connect to.
-            // TODO: What if steam makes the original host a client?
-
-            transportSteamworks.CloseP2PSessions();
-
-            transportSteamworks.CreateClientSocket();
-
-            // TODO: Need to go through the handshake process
-            //       plus fast-track it on the host side for existing
-            //       steam IDs.
-        }
+            ConnectToNewHost(transportSteamworks);
     }
 
     private void OnDestroy()
     {
         if (Singleton == this) Singleton = null;
+    }
+
+    /// <summary>
+    ///     Turns this client into a host (only over steam networking).
+    /// </summary>
+    private void SwitchToHost(TransportSteamworks transportSteamworks)
+    {
+        ServerMain._RegisterServerReceivers();
+
+        Net.type = Net.NetType.Host;
+
+        var ply = transportSteamworks.SteamIDToNetPlayerDict[KSteam.GetLocalUserSteamID().m_SteamID];
+        ply.is_host = true;
+        ply.is_local = true;
+        ply.server_plrstate = new Server_PlayerState(ply);
+
+        NetPlayer.LOCAL_PLAYER = ply;
+
+        // TODO: Figure out a better place to spawn players.
+        Body_PlaceBody_MultiplayerPatch.has_spawn_location = true;
+        Body_PlaceBody_MultiplayerPatch.spawnlocation = ply.body.transform.position;
+
+        HardcodedServer.CurHostID = ply.clientId;
+
+        FixSyncObjects();
+
+        if (transportSteamworks.listenSocket != HSteamListenSocket.Invalid)
+        {
+            if (transportSteamworks.is_steamserver)
+                SteamGameServerNetworkingSockets.CloseListenSocket(transportSteamworks.listenSocket);
+            else
+                SteamNetworkingSockets.CloseListenSocket(transportSteamworks.listenSocket);
+        }
+
+        transportSteamworks.CloseP2PSessions();
+
+        transportSteamworks.CreateServerSocket();
+    }
+
+    /// <summary>
+    ///     Connects to the newly elected host (only over steam networking)
+    /// </summary>
+    private void ConnectToNewHost(TransportSteamworks transportSteamworks)
+    {
+        // TODO: What if steam makes the original host a client?
+
+        transportSteamworks.CloseP2PSessions();
+
+        transportSteamworks.CreateClientSocket();
+
+        // TODO: Need to go through the handshake process
+        //       plus fast-track it on the host side for existing
+        //       steam IDs.
+    }
+
+    /// <summary>
+    ///     Turns client-sided objects into host sync objects.
+    ///     This can't run unless the networking is in host mode.
+    /// </summary>
+    private void FixSyncObjects()
+    {
+        // TODO: Are all objects available, or do we need to full sync on interval?
+
+        foreach (var item in FindObjectsOfType<Item>()) Item_Start_MultiplayerPatch.Postfix(item);
+
+        foreach (var building in FindObjectsOfType<BuildingEntity>())
+        {
+            BuildingEntity_Start_MultiplayerPatch.Prefix(building);
+            BuildingEntity_Start_MultiplayerPatch.Postfix(building);
+        }
+
+        var sync = NewCoolerObjectPacketWriteReadSystem.inst;
+        foreach (var clientObj in sync.client_objects)
+        {
+            if (sync.server_objects.ContainsKey(clientObj.Key)) continue;
+            sync.server_objects[clientObj.Key] =
+                new CoolSyncSubSystemForObjects.Server_Object
+                {
+                    netId = clientObj.Value.netId,
+                    real_obj = clientObj.Value.real_obj
+                };
+        }
+
+        sync.client_objects.Clear();
+
+        // TODO: There's probably a bunch of other similar patches that need to be called.
     }
 }
